0df2b5bf940b99161301178d35727a02bae09459
Кирилл Князев <rizorpolder@gmail.com>
2019-04-15 22:41:12 +0300

    Добавьте файлы проекта.

---
+++ CrmUi/CustomerForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace CrmUi
+{
+    partial class CustomerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(13, 13);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(74, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Введите Имя";
+            // 
+            // textBox2
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(104, 10);
+            this.textBox1.Name = "textBox2";
+            this.textBox1.Size = new System.Drawing.Size(86, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button2.Location = new System.Drawing.Point(183, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CustomerForm
+            // 
+            this.ClientSize = new System.Drawing.Size(270, 208);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelName);
+            this.Name = "CustomerForm";
+            this.Load += new System.EventHandler(this.CustomerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+       
+        
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ CrmUi/Main.cs
@@ -0,0 +1,49 @@
+﻿using CrmBl.Model;
+using System.Windows.Forms;
+
+namespace CrmUi
+{
+    public partial class Main : Form
+    {
+        CrmContext db;
+        public Main()
+        {
+            InitializeComponent();
+            db=new CrmContext();
+        }
+      
+        private void ProductToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            var catalogProduct = new Catalog<Product>(db.Products);
+            catalogProduct.Show();
+        }
+
+        private void SellerToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            var catalogSeller = new Catalog<Seller>(db.Sellers);
+            catalogSeller.Show();
+        }
+
+        private void CustomerToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            var catalogCustomer = new Catalog<Customer>(db.Customers);
+            catalogCustomer.Show();
+        }
+
+        private void CheckToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            var catalogCheck = new Catalog<Check>(db.Checks);
+            catalogCheck.Show();
+        }
+
+        private void CustomerAddToolStripMenuItem2_Click(object sender, System.EventArgs e)
+        {
+            var form = new CustomerForm();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                db.Customers.Add(form.Customer);
+                db.SaveChanges();
+            }
+        }
+    }
+}
