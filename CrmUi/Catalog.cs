6e906216cfa19ebd60cb5108ad16a791de79d347
Кирилл Князев <rizorpolder@gmail.com>
2019-04-18 16:26:56 +0300

    55:23

--- CrmUi/Catalog.cs
+++ CrmUi/Catalog.cs
@@ -1,21 +1,43 @@
 ﻿using System;
 using System.Data.Entity;
 using System.Windows.Forms;
+using CrmBl.Model;
 
 namespace CrmUi
 {
     public partial class Catalog<T> : Form where T : class
     {
-
-
-        public Catalog(DbSet<T> set)
+        private CrmContext db;
+        public Catalog(DbSet<T> set, CrmContext db)
         {
             InitializeComponent();
+            this.db = db;
+            set.Load();
+            //dataGridView.DataSource = db.Set<T>().Local.ToBindingList();
             dataGridView.DataSource = set.Local.ToBindingList();
         }
 
         private void Catalog_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (typeof(T) == typeof(Product))
+            {
+                //var form = new CustomerForm();
+                //if (form.ShowDialog() == DialogResult.OK)
+                //{
+                //    db.Customers.Add(form.Customer);
+                //    db.SaveChanges();
+                //}
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var id = dataGridView.SelectedRows[0].Cells[0].Value;
 
         }
     }
